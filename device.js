0b80b82078b0ff16c4952a1a7f3f35c053163ce1
ddaf <banelos@gmail.com>
2014-12-10 15:29:58 +0100

    Added self as a field variable in init and removed self vars from functions

--- device.js
+++ device.js
@@ -6,8 +6,7 @@ var debug = require('debug')('Device');
 
 var Device = function(options) {
     events.EventEmitter.call(this);
-    var self = this;
-    self.config = options;
+    this.config = options;
     this.init();
 };
 
@@ -15,8 +14,6 @@ exports.Device = Device;
 util.inherits(Device, events.EventEmitter);
 
 Device.prototype.connect = function(callback) {
-    var self = this;
-    
     self.client.connect(self.host, function() {
         debug('connected, launching app ...');
         self.client.launch(DefaultMediaReceiver, function(err, player) {
@@ -46,16 +43,13 @@ Device.prototype.connect = function(callback) {
 };
 
 Device.prototype.init = function() {
-    var self = this;
-    
+    self = this;
     self.client = new Client();
     self.host = self.config.addresses[0];
     self.playing = false;
 };
 
 Device.prototype.play = function(resource, n, callback) {
-    var self = this;
-
     options = { autoplay: true };
 
     if (typeof(resource) === 'string'){
@@ -115,8 +109,6 @@ Device.prototype.play = function(resource, n, callback) {
 };
 
 Device.prototype.getStatus = function(callback) {
-    var self = this;
-    
     self.player.getStatus(function(err, status) {
         if (err) {
             console.log("getStatus error: %s", err.message);
@@ -127,8 +119,6 @@ Device.prototype.getStatus = function(callback) {
 };
 
 Device.prototype.seek = function(seconds, callback) {
-    var self = this;
-
     // Retrieve updated status just before seek
     self.getStatus(function(newStatus) {
         newCurrentTime = newStatus.currentTime + seconds;
@@ -139,35 +129,25 @@ Device.prototype.seek = function(seconds, callback) {
 };
 
 Device.prototype.pause = function(callback) {
-    var self = this;
-
     self.playing = false;
     self.player.pause(callback);
 };
 
 Device.prototype.setVolume = function(volume, callback) {
-    var self = this;
-
     self.client.setVolume(volume, callback);
 };
 
 Device.prototype.unpause = function(callback) {
-    var self = this;
-    
     self.playing = true;
     self.player.play(callback);
 };
 
 Device.prototype.stop = function(callback) {
-    var self = this;
-    
     self.playing = false;
     self.player.stop(callback);
 };
 
 Device.prototype.subtitlesOff = function(callback) {
-    var self = this;
-    
     self.player.media.sessionRequest({
         type: 'EDIT_TRACKS_INFO',
         activeTrackIds: [] // turn off subtitles.
@@ -178,8 +158,6 @@ Device.prototype.subtitlesOff = function(callback) {
 };
 
 Device.prototype.changeSubtitles = function(num, callback) {
-    var self = this;
-    
     self.player.media.sessionRequest({
         type: 'EDIT_TRACKS_INFO',
         activeTrackIds: [num] // turn off subtitles.
@@ -190,8 +168,6 @@ Device.prototype.changeSubtitles = function(num, callback) {
 };
 
 Device.prototype.changeSubtitlesSize = function(num, callback) {
-    var self = this;
-    
     var newStyle = self.subtitles_style;
     newStyle.fontScale = num;
     self.player.media.sessionRequest({
